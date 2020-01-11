c2b222d92edfcf9d46fd63b31b7664328efde0cc
Shayne Clausson <shayne.clausson@extendaretail.com>
2020-01-11 21:17:03 +0100

    Add outputs

---
+++ modules/project/outputs.tf
@@ -0,0 +1,11 @@
+output project_id {
+  value = module.project_factory.project_id
+}
+
+output project_name {
+  value = module.project_factory.project_name
+}
+
+output service_account_email {
+  value = module.project_factory.service_account_email
+}
