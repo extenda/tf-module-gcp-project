2fb2ed3171a1b94ac7e91f68676f1dda681f5ca5
Shayne Clausson <shayne.clausson@extendaretail.com>
2020-01-12 19:49:48 +0100

    Fix syntax errors

--- modules/ci-cd-service-account/outputs.tf
+++ modules/ci-cd-service-account/outputs.tf
@@ -2,6 +2,6 @@ output private_key {
   value = base64decode(google_service_account_key.key_json.private_key)
 }
 
-output privte_key_encoded {
+output private_key_encoded {
   value = google_service_account_key.key_json.private_key
 }
