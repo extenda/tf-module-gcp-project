117f89bad42247693e2d0e7faa3e6d425e35d741
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2020-02-06 19:01:11 +0100

    Fix for optional service-account creation

--- modules/service-account/main.tf
+++ modules/service-account/main.tf
@@ -7,7 +7,7 @@ resource "google_service_account" "sa" {
 }
 
 resource "google_project_iam_member" "project" {
-  for_each = var.iam_roles
+  for_each = length(google_service_account.sa) > 0 ? var.iam_roles : {}
   project  = var.project_id
   role     = each.value
   member   = "serviceAccount:${google_service_account.sa[0].email}"
@@ -16,6 +16,8 @@ resource "google_project_iam_member" "project" {
 }
 
 resource "google_service_account_key" "key_json" {
+  count = var.create_service_account ? 1 : 0
+
   service_account_id = google_service_account.sa[0].id
 
   depends_on = [google_service_account.sa[0]]
--- modules/service-account/outputs.tf
+++ modules/service-account/outputs.tf
@@ -1,14 +1,14 @@
 output email {
   description = "The service account email"
-  value       = google_service_account.sa[0].email
+  value       = length(google_service_account.sa) > 0 ? google_service_account.sa[0].email : null
 }
 
 output private_key {
   description = "The service account JSON key"
-  value       = base64decode(google_service_account_key.key_json.private_key)
+  value       = length(google_service_account.sa) > 0 ? base64decode(google_service_account_key.key_json[0].private_key) : null
 }
 
 output private_key_encoded {
   description = "The base64 encoded service account JSON key"
-  value       = google_service_account_key.key_json.private_key
+  value       = length(google_service_account.sa) > 0 ? google_service_account_key.key_json[0].private_key : null
 }
