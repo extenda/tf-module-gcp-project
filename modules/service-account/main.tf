4d938f129043dc757db59513c7593896d06001af
Aleksandr Skoriy <al.skoriy@gmail.com>
2020-04-27 14:43:01 +0300

    Refacror module to avoid lists in resource definition

--- modules/service-account/main.tf
+++ modules/service-account/main.tf
@@ -1,10 +1,9 @@
 locals {
   service_roles = flatten([for service_key, service in var.services : [
     for role_key, role in service.iam_roles : {
-      service_key = service_key
-      role_key    = role_key
-      service_id  = var.create_service_account == true ? google_service_account.sa[service_key].account_id : ""
-      role_id     = role
+      name        = service.name
+      role        = role
+      service_id  = var.create_service_account == true ? google_service_account.sa[service.name].account_id : ""
     }
     ]
   ])
@@ -12,47 +11,47 @@ locals {
 
 resource "google_service_account" "sa" {
   for_each = {
-    for key, value in var.services :
-    key => key
+    for service in var.services :
+    service.name => service
     if var.create_service_account == true
   }
-  account_id   = var.services[each.value].name
-  display_name = "${var.services[each.value].name} Service Account"
+  account_id   = each.key
+  display_name = "${each.key} Service Account"
   project      = var.project_id
 }
 
 resource "google_project_iam_member" "project_roles" {
   for_each = {
     for service in local.service_roles :
-    "${service.service_key}.${service.role_key}" => service
+    "${service.name}.${service.role}" => service
     if var.create_service_account == true && var.create_service_group == false
   }
   project = var.project_id
-  role    = each.value.role_id
-  member  = "serviceAccount:${google_service_account.sa[each.value.service_key].email}"
+  role    = each.value.role
+  member  = "serviceAccount:${google_service_account.sa[each.value.name].email}"
 
   depends_on = [google_service_account.sa]
 }
 
 resource "google_service_account_key" "key_json" {
   for_each = {
-    for key, value in var.services :
-    key => key
+    for service in var.services :
+    service.name => service
     if var.create_service_account == true
   }
-  service_account_id = "projects/${var.project_id}/serviceAccounts/${var.services[each.value].name}@${var.project_id}.iam.gserviceaccount.com"
+  service_account_id = "projects/${var.project_id}/serviceAccounts/${each.key}@${var.project_id}.iam.gserviceaccount.com"
   depends_on = [google_service_account.sa]
 }
 
 resource "gsuite_group" "service_group" {
   for_each = {
-    for key, value in var.services :
-    key => key
+    for service in var.services :
+    service.name => service
     if var.create_service_account == true && var.create_service_group == true
   }
-  email       = "${var.service_group_name}-${var.services[each.value].name}@${var.domain}"
-  name        = "${var.service_group_name}-${var.services[each.value].name}"
-  description = "${var.services[each.value].name} GSuite Group"
+  email       = "${var.service_group_name}-${each.key}@${var.domain}"
+  name        = "${var.service_group_name}-${each.key}"
+  description = "${each.key} GSuite Group"
 
   depends_on = [google_service_account.sa]
 }
@@ -60,12 +59,12 @@ resource "gsuite_group" "service_group" {
 
 resource "gsuite_group_member" "service_account_sa_group_member" {
   for_each = {
-    for key, value in var.services :
-    key => key
+    for service in var.services :
+    service.name => service
     if var.create_service_account == true && var.create_service_group == true
   }
-  group = "${var.service_group_name}-${var.services[each.value].name}@${var.domain}"
-  email = google_service_account.sa[each.value].email
+  group = "${var.service_group_name}-${each.key}@${var.domain}"
+  email = google_service_account.sa[each.key].email
   role  = "MEMBER"
 
   depends_on = [gsuite_group.service_group]
@@ -73,11 +72,11 @@ resource "gsuite_group_member" "service_account_sa_group_member" {
 
 resource "gsuite_group_member" "clan_group_member" {
   for_each = {
-    for key, value in var.services :
-    key => key
+    for service in var.services :
+    service.name => service
     if var.create_service_account == true && var.create_service_group == true && var.env_name == "staging"
   }
-  group = "${var.service_group_name}-${var.services[each.value].name}@${var.domain}"
+  group = "${var.service_group_name}-${each.key}@${var.domain}"
   email = "${var.clan_gsuite_group}@${var.domain}"
   role  = "MEMBER"
 
@@ -88,12 +87,12 @@ resource "gsuite_group_member" "clan_group_member" {
 resource "google_project_iam_member" "service_group_roles" {
   for_each = {
     for service in local.service_roles :
-    "${service.service_key}.${service.role_key}" => service
+    "${service.name}.${service.role}" => service
     if var.create_service_account == true && var.create_service_group == true
   }
   project = var.project_id
-  role    = each.value.role_id
-  member  = "group:${var.service_group_name}-${var.services[each.value.service_key].name}@${var.domain}"
+  role    = each.value.role
+  member  = "group:${var.service_group_name}-${each.value.name}@${var.domain}"
 
   depends_on = [gsuite_group.service_group]
 }
