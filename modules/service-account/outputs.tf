353255e4b8b4806b9060b3a857b60c1cd1bcaaa4
Aleksandr Skoriy <al.skoriy@gmail.com>
2020-04-27 16:11:38 +0300

    fix output generation

--- modules/service-account/outputs.tf
+++ modules/service-account/outputs.tf
@@ -1,16 +1,16 @@
 output email {
   description = "The service account emails"
   value = {
-    for sa in google_service_account.sa :
-    sa.account_id => sa.email
+    for name, sa in google_service_account.sa :
+    name => sa.email
   }
 }
 
 output private_key {
   description = "The service account JSON keys"
   value = {
-    for key in google_service_account_key.key_json :
-    key.service_account_id => base64decode(key.private_key)
+    for name, key in google_service_account_key.key_json :
+    name => base64decode(key.private_key)
   }
   sensitive = true
 }
@@ -18,15 +18,15 @@ output private_key {
 output private_key_encoded {
   description = "The base64 encoded service account JSON keys"
   value = {
-    for key in google_service_account_key.key_json :
-    key.service_account_id => key.private_key
+    for name, key in google_service_account_key.key_json :
+    name => key.private_key
   }
   sensitive = true
 }
 
 output gsuite_group_email {
   value = {
-    for group in gsuite_group.service_group :
+    for key, group in gsuite_group.service_group :
     group.name => group.email
   }
 }
