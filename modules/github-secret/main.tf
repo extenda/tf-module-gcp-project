9a6a194c4c9239d7f1c5a04a3087b343e4de1d86
Anastasiia Kovtun <anastasiia.kovtun@extendaretail.com>
2022-06-06 12:00:31 +0100

    feat: Add dependabot secrets

--- modules/github-secret/main.tf
+++ modules/github-secret/main.tf
@@ -7,7 +7,6 @@ data "google_secret_manager_secret_version" "github_token" {
 }
 
 provider "github" {
-  version      = "~> 2.0"
   token        = (var.github_token != "") ? var.github_token : data.google_secret_manager_secret_version.github_token[0].secret_data
   organization = var.github_organization
 }
@@ -19,6 +18,20 @@ data "github_actions_public_key" "repo_key" {
 }
 
 resource "github_actions_secret" "gcloud_secret" {
+  for_each = var.create_secret ? toset(var.repositories) : toset([])
+
+  repository      = each.key
+  secret_name     = var.secret_name
+  plaintext_value = var.secret_value
+}
+
+data "github_dependabot_public_key" "repo_key_dependabot" {
+  for_each = var.create_secret ? toset(var.repositories) : toset([])
+
+  repository = each.key
+}
+
+resource "github_dependabot_secret" "gcloud_secret_dependabot" {
   for_each = var.create_secret ? toset(var.repositories) : toset([])
 
   repository      = each.key
