3a1729dd38d256b52dfe0d52d5adcfc61128e2cf
Anastasiia Kovtun <anastasiia.kovtun@extendaretail.com>
2023-04-27 14:04:59 +0100

    fix: Revert wrong changes

--- modules/external-project-iam-roles/main.tf
+++ modules/external-project-iam-roles/main.tf
@@ -8,6 +8,27 @@ locals {
   ])
 }
 
+resource "google_project_iam_member" "project_service_roles" {
+  for_each = {
+    for service in local.service_roles :
+    "${service.name}.${service.role}" => service
+    if var.service_account_exists == true && var.project_id != ""
+  }
+  project = var.project_id
+  role    = each.value.role
+  member  = "serviceAccount:${each.value.name}@${var.project_id}.iam.gserviceaccount.com"
+
+  depends_on = [var.sa_depends_on]
+}
+
+resource "google_project_iam_member" "project_gke_role" {
+  count = var.project_type == "clan_project" ? 1 : 0
+
+  project = var.project_id
+  role    = "projects/${var.project_id}/roles/cicd.gke.manager"
+  member  = "serviceAccount:${var.service_account}"
+}
+
 resource "google_project_iam_member" "parent_project_service_roles" {
   for_each = {
     for service in local.service_roles :
@@ -35,7 +56,6 @@ resource "google_project_iam_member" "parent_project_gke_role" {
   project = var.parent_project_id
   role    = "projects/${var.parent_project_id}/roles/cicd.gke.manager"
   member  = "serviceAccount:${var.service_account}"
-
 }
 
 resource "google_project_iam_member" "gcr_project_roles" {
@@ -55,7 +75,7 @@ resource "google_project_iam_member" "dns_project_roles" {
 }
 
 resource "google_project_iam_custom_role" "gke_custom_role" {
-  count = var.project_type == "tribe_project" ? 1 : 0
+  count = var.project_type == "tribe_project" || var.project_type == "clan_project" ? 1 : 0
   
   project     = var.project_id
   role_id     = "cicd.gke.manager"
@@ -65,7 +85,7 @@ resource "google_project_iam_custom_role" "gke_custom_role" {
 }
 
 resource "google_project_iam_member" "token_creator_project_role" {
-  count = var.env_name == "staging" && var.project_type != "tribe_project"? 1 : 0
+  count = var.env_name == "staging" && var.project_type != "tribe_project" ? 1 : 0
 
   project = var.project_id
   role    = "roles/iam.serviceAccountTokenCreator"
